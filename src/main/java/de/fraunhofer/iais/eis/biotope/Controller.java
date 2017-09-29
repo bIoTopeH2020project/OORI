b646dd392b4f383ed0f11f2be10cf40115e172df
christianmader <christian.mader@univie.ac.at>
2017-09-29 16:53:53 +0200

    added extraction of o-df data from o-mi response message

--- src/main/java/de/fraunhofer/iais/eis/biotope/Controller.java
+++ src/main/java/de/fraunhofer/iais/eis/biotope/Controller.java
@@ -1,12 +1,24 @@
 package de.fraunhofer.iais.eis.biotope;
 
+import org.apache.http.HttpStatus;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.rio.RDFWriter;
 import org.eclipse.rdf4j.rio.turtle.TurtleWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSSerializer;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 public class Controller {
@@ -19,6 +31,7 @@ public class Controller {
     @RequestMapping(value = "/toRDF", method = RequestMethod.POST)
     @ResponseBody
     public String toRDF(@RequestBody String omiOdfXmlResponse, String omiNodeHostName)
+        throws IOException, SAXException
     {
         if (baseUri == null) baseUri = "http://localhost/";
         String odfStructure = extractOdfContent(omiOdfXmlResponse);
@@ -34,9 +47,25 @@ public class Controller {
      * @param omiOdfXmlResponse The whole O-MI/O-DF response
      * @return The part of the XML response which has been passed as a parameter and which only contains O-DF information
      */
-    private String extractOdfContent(String omiOdfXmlResponse) {
-        // todo: implement me!
-        return omiOdfXmlResponse;
+    private String extractOdfContent(String omiOdfXmlResponse) throws IOException, SAXException {
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new ByteArrayInputStream(omiOdfXmlResponse.getBytes(StandardCharsets.UTF_8)));
+        return innerXml(doc.getElementsByTagName("msg").item(0));
+    }
+
+    private String innerXml(Node node) {
+        DOMImplementationLS lsImpl = (DOMImplementationLS)node.getOwnerDocument().getImplementation().getFeature("LS", "3.0");
+        LSSerializer lsSerializer = lsImpl.createLSSerializer();
+        lsSerializer.getDomConfig().setParameter("xml-declaration", false);
+        NodeList childNodes = node.getChildNodes();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node childNode = childNodes.item(i);
+            if (childNode.getTextContent().trim().isEmpty()) continue;
+
+            sb.append(lsSerializer.writeToString(childNodes.item(i)));
+        }
+        return sb.toString();
     }
 
     private String rdfModelToTurtle(Model model) {
